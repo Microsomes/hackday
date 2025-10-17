793e9947c796f6754e2e22aa5838a09168f2c96a
Tayyab Javed <tayyab@appoly.co.uk>
2025-10-17 11:07:20 +0100

    removed dead code we dont need

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -11,18 +11,13 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
 import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: dashboard(),
-        icon: LayoutGrid,
-    },
+    
 ];
 
 const footerNavItems: NavItem[] = [
@@ -45,9 +40,7 @@ const footerNavItems: NavItem[] = [
             <SidebarMenu>
                 <SidebarMenuItem>
                     <SidebarMenuButton size="lg" as-child>
-                        <Link :href="dashboard()">
                             <AppLogo />
-                        </Link>
                     </SidebarMenuButton>
                 </SidebarMenuItem>
             </SidebarMenu>
--- resources/js/pages/game/Game.vue
+++ resources/js/pages/game/Game.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-    import AppLayout from '@/layouts/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
         <Head title="Breakfast Battle!" />
+
+        <div>
+            the game (::)
+        </div>
 </template>
